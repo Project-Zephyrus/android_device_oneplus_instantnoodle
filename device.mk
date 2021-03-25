ddc1cef3387b42250e24401ab2d57e37120e5fc7
LuK1337 <priv.luk@gmail.com>
2021-03-25 19:06:23 +0100

    instantnoodle: Import RoW/VZW sensor cfgs and guard them with "project"
    This should allow both regular and vzw instantnoodle
    variants to load proper sensor configuration files.
    
    Notes as to how and why this works:
    
    Basically, each config file has a "config" node that describes
    the requirements for the loading sensor configuration data, e.g:
      "config":{
        "hw_platform": ["QRD", "MTP", "Dragon", "Surf", "HDK", "IDP"],
        "soc_id": ["356"],
        "project": ["19821", "19855", "19868"]
      }
    
    The list of acceptable requirements can be extended by modifying
    /vendor/etc/sensors/sns_reg_config. You can add the following line:
    "file=hw_id=/sys/project_info/hw_id" to make it load file and let
    you filter configs by `hw_id` value.
    
    Change-Id: Ibf36222ff0cc3a5d9abcc55e192b9089425cdebe

--- device.mk
+++ device.mk
@@ -30,6 +30,14 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     OnePlus8WifiOverlay
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/row/kona_bmi26x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_bmi26x_row.json \
+    $(LOCAL_PATH)/configs/sensors/row/kona_mmc5603x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_mmc5603x_0_row.json \
+    $(LOCAL_PATH)/configs/sensors/vzw/kona_T0_bmi26x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_T0_bmi26x_0_vzw.json \
+    $(LOCAL_PATH)/configs/sensors/vzw/kona_bmi26x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_bmi26x_0_vzw.json \
+    $(LOCAL_PATH)/configs/sensors/vzw/kona_mmc5603x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kona_mmc5603x_0_vzw.json
+
 # Inherit from oneplus sm8250-common
 $(call inherit-product, device/oneplus/sm8250-common/common.mk)
 
